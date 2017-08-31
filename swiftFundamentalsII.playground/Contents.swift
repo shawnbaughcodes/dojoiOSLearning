b98d3e58aba1fccf7ee308e86335386a351433e7
Shawn Baugh II <shawnbaughcreative@gmail.com>
2017-08-31 15:15:42 -0500

    Finished Fundamentals 2

---
+++ swiftFundamentalsII.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var number = 2
+
+for i in 1...255{
+	print(i)
+}
+
+for i in 1...100 {
+	if i % 3 == 0 {
+		print("Fizz")
+	} else if i % 5 == 0 {
+		print("Buzz")
+	}
+}
+
+
