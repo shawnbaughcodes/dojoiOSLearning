180e0d0011466ce71160614af62b5fb62a5fe549
Shawn Baugh II <shawnbaughcreative@gmail.com>
2017-09-07 13:40:47 -0500

    Started an assignment

---
+++ ttt/ttt/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  ttt
+//
+//  Created by Shawn Baugh II on 9/6/17.
+//  Copyright © 2017 Shawn Baugh Creative. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	
+	@IBAction func button1(_ sender: UIButton) {
+		print("button1 pressed")
+	}
+	@IBAction func button2(_ sender: UIButton) {
+		print("button2 pressed")
+	}
+	@IBAction func button3(_ sender: UIButton) {
+		print("button3 pressed")
+	}
+	@IBAction func button4(_ sender: UIButton) {
+		print("button4 pressed")
+	}
+	@IBAction func button5(_ sender: UIButton) {
+		print("button5 pressed")
+	}
+	@IBAction func button6(_ sender: UIButton) {
+		print("button6 pressed")
+	}
+	@IBAction func button7(_ sender: UIButton) {
+		print("button7 pressed")
+	}
+	@IBAction func button8(_ sender: UIButton) {
+		print("button8 pressed")
+	}
+	@IBAction func button9(_ sender: UIButton) {
+		print("button9 pressed")
+	}
+	func turnRed() {
+		
+	}
+	func turnBlue() {
+		<#function body#>
+	}
+	func boxColor() {
+		
+	}
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
